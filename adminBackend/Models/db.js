fd00ea3585ae17db0e396032a43ba4f138249190
Ahadinash07 <ahadinashhamza.gmail.com>
2025-04-28 16:28:15 +0530

    update some files

--- adminBackend/Models/db.js
+++ adminBackend/Models/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: "localhost",
+    user: "verceluser",
+    password: "strongpassword",
+    database: "xe_comm",
     port: 3306
 });
 
