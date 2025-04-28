482f90cacb034a04bb3cb898c8c580bbb4d4aee8
Ahadinash07 <ahadinashhamza.gmail.com>
2025-04-28 15:51:48 +0530

    app.js update cors

--- adminBackend/app.js
+++ adminBackend/app.js
@@ -19,7 +19,8 @@ const AddressRoute = require('./Routes/AddressRoute/AddressRoute');
 const app = express();
 env.config();
 app.use(cors({
-    origin: ['http://localhost:5173/', 'https://ecomm-frontend-mu.vercel.app/'],
+    // origin: ['http://localhost:5173', 'https://ecomm-frontend-mu.vercel.app'],
+    origin: ['http://localhost:5173', 'https://e-shop-client.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
